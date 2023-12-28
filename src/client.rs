0333d1ec2b228d1be5851e00a6393d1d5512b169
phon <hassanjaved186@gmail.com>
2023-12-28 14:04:44 +0000

    feat: add ip to sent message

--- src/client.rs
+++ src/client.rs
@@ -3,31 +3,29 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::env::args;
 use std::thread;
-use std::io::stdin;
+use std::io::{stdin, stdout};
+use std::str;
 
 fn main() {
     let server_addr = args().nth(1).expect("Please provide server IP");
     println!("{server_addr}");
 
     let mut buf: [u8; 256] = [0; 256];
-
-    println!("{server_addr}");
-    let mut stream = TcpStream::connect(&server_addr).expect(server_addr.as_str());
-    // let mut strin: &TcpStream = &stream;
-
-        loop {
-            match stream.read(&mut buf) {
-                Ok(n) => {
-                    let message: String = buf[0..n]
-                    .iter()
-                    .map(|&b| {
-                        b.to_ascii_lowercase() as char
-                    }).collect();
-                    
-                    println!("{message:?}")
-                },
-                Err(e) => panic!("{e}"),
-            }
+    let mut stream = TcpStream::connect(&server_addr).expect("Failed to connect");
+    stream.write(b"hello").unwrap();
+    loop {
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                let message: String = buf[0..n]
+                .iter()
+                .map(|&b| {
+                    b.to_ascii_lowercase() as char
+                }).collect();
+                
+                stream.write(message.as_bytes()).unwrap()
+            },
+            Err(e) => panic!("{e}"),
+        };
         let mut input = String::new();
         match stdin().read_line(&mut input) {
             Ok(0) => break,
@@ -36,10 +34,11 @@ fn main() {
                 println!("{n} bytes read");
                 println!("{input}");
                 stream.write_all(input.as_bytes()).expect("Failed to read buffer to stream");
+                input.clear();
             },
 
             Err(e) => panic!("Could not read from stdin: {e}"),            
-        }
+        };
     }
 
 }
