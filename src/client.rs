808fd53103d780c8c028862d3c1e43c227537cc5
phon <hassanjaved186@gmail.com>
2023-12-31 13:03:02 +0000

    feat: basic client implementation

--- src/client.rs
+++ src/client.rs
@@ -2,9 +2,8 @@ use core::panic;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::env::args;
+use std::io::stdin;
 use std::thread;
-use std::io::{stdin, stdout};
-use std::str;
 
 fn main() {
     let server_addr = args().nth(1).expect("Please provide server IP");
@@ -12,33 +11,32 @@ fn main() {
 
     let mut buf: [u8; 256] = [0; 256];
     let mut stream = TcpStream::connect(&server_addr).expect("Failed to connect");
-    stream.write(b"hello").unwrap();
+    let mut input_stream = stream.try_clone().expect("Failed to clone socket");
+    
+    thread::spawn(move || { 
+        loop {
+            let mut input = String::new();
+            match stdin().read_line(&mut input) {
+                Ok(0) => break,
+                
+                Ok(_) => {
+                    input_stream.write(input.as_bytes()).expect("Failed to read buffer to stream");
+                    input.clear();
+                },
+                
+                Err(e) => panic!("Could not read from stdin: {e}"),            
+            };
+        }});
+
     loop {
         match stream.read(&mut buf) {
-            Ok(n) => {
-                let message: String = buf[0..n]
-                .iter()
-                .map(|&b| {
-                    b.to_ascii_lowercase() as char
-                }).collect();
-                
-                stream.write(message.as_bytes()).unwrap()
-            },
-            Err(e) => panic!("{e}"),
-        };
-        let mut input = String::new();
-        match stdin().read_line(&mut input) {
-            Ok(0) => break,
+        Ok(n) => {
+            let message: String = String::from_utf8(buf[0..n].to_vec()).unwrap();
+            
+            println!("{message}");
+        },
 
-            Ok(n) => { 
-                println!("{n} bytes read");
-                println!("{input}");
-                stream.write_all(input.as_bytes()).expect("Failed to read buffer to stream");
-                input.clear();
-            },
-
-            Err(e) => panic!("Could not read from stdin: {e}"),            
-        };
-    }
+        Err(e) => panic!("{e}"),
+    }};
 
 }
