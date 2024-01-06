d90e02c83430b486a290f3275fb4ee8d632e7174
phon <hassanjaved186@gmail.com>
2024-01-06 23:35:27 +0000

    feat(go server): generate output correctly

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -20,9 +20,6 @@ func main() {
 		return
 	}
 	fmt.Println("Listening at 127.0.0.1:6969")
-
-	// buffer := bufio.NewReader(stream)
-
 	defer stream.Close()
 
 	for {
@@ -33,14 +30,14 @@ func main() {
 		}
 		connections = append(connections, conn)
 		wg.Add(1)
-		go handleConnection(conn, &wg, connections)
+		go handleConnection(conn, &wg, &connections)
 
 	}
 }
 
-func handleConnection(conn net.Conn, wg *sync.WaitGroup, connections []net.Conn) {
+func handleConnection(conn net.Conn, wg *sync.WaitGroup, connections *[]net.Conn) {
 	defer wg.Done()
-	fmt.Println(conn, connections)
+	fmt.Println("Accepted connection from:", conn.RemoteAddr())
 
 	buffer := make([]byte, 256)
 
@@ -49,6 +46,11 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup, connections []net.Conn)
 		if err != nil {
 			if err == io.EOF {
 				fmt.Println("Connection has been closed by", conn.RemoteAddr())
+				// for i, connection := range *connections {
+				// 	if connection.RemoteAddr() != conn.RemoteAddr() {
+				// 		*connections = append((*connections)[:i], (*connections)[i+1:]...)
+				// 	}
+				// }
 				// TODO: Remove conn from connections[]
 				return
 			}
@@ -58,16 +60,14 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup, connections []net.Conn)
 		}
 		fmt.Println("Received", buffer[:n], "from", conn.RemoteAddr())
 		if n != 0 {
-			var out []byte
-			if buffer[n] == 10 {
-				out = buffer[:n-1]
-			} else {
-				out = buffer[:n]
+			out, valid := generateOutput(n, buffer, conn)
+			if !valid {
+				continue
 			}
 			fmt.Println("Sending", out, "from", conn.RemoteAddr())
-			for _, connection := range connections {
+			for _, connection := range *connections {
 				if conn.RemoteAddr() != connection.RemoteAddr() {
-					_, err = conn.Write(out)
+					_, err = connection.Write(out)
 					if err != nil {
 						fmt.Println("Error writing to client:", err)
 					}
@@ -75,4 +75,22 @@ func handleConnection(conn net.Conn, wg *sync.WaitGroup, connections []net.Conn)
 			}
 		}
 	}
+}
+
+func generateOutput(n int, buffer []byte, conn net.Conn) ([]byte, bool) {
+	fmt.Println(buffer[n], "or", buffer[n-1])
+	if n == 1 && buffer[n] == 10 {
+		return []byte(""), false
+	}
+
+	var output []byte
+	addr := []byte(conn.RemoteAddr().String() + ": ")
+
+	output = append(output, addr...)
+	if buffer[n] == 10 || buffer[n-1] == 10 {
+		output = append(output, buffer[:n-1]...)
+	} else {
+		output = append(output, buffer[:n]...)
+	}
+	return output, true
 }
